e12281be37bb3a22f3038d1427d1c4436b6d2fbb
agilllkw-alt <agilllkw@gmail.com>
2026-08-30 01:33:46 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,8 @@
-const CACHE_NAME = 'carade-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo-unhas.png',
-  './logo-bem.png',
-  './logo-gowa.png'
-];
-
-// Tahap Instalasi: Menyimpan file ke dalam cache HP
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Service Worker: Caching files');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (e) => {
+  console.log('SW terpasang');
 });
 
-// Tahap Aktivasi: Membersihkan cache lama jika ada update
-self.addEventListener('activate', event => {
-  console.log('Service Worker: Activated');
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Strategi Fetch: Ambil dari internet dulu, jika offline ambil dari cache
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+self.addEventListener('fetch', (e) => {
+  // Handler fetch kosong agar syarat PWA Chrome terpenuhi
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
